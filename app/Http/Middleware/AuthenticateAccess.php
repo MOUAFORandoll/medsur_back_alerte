f49eaf9827cdc300edda3b4be7fe12a57662b9d0
Berthold FEUJO <feujio@medicasure.com>
2023-06-08 10:44:57 +0100

    Log des autorisations

--- app/Http/Middleware/AuthenticateAccess.php
+++ app/Http/Middleware/AuthenticateAccess.php
@@ -15,6 +15,7 @@ class AuthenticateAccess
      */
     public function handle(Request $request, Closure $next): Response
     {
+        \Log::alert($request->header());
         $credentials = [
             'grant_type' => 'password',
             'client_id' => 2,
